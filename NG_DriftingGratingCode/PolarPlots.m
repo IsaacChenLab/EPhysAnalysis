6040ba0adcad28bc52706c01afe0587de8cd7d47
NickGoel <nckgoel@gmail.com>
2017-08-04 12:54:09 -0400

    Update PolarPlots.m

--- NG_DriftingGratingCode/PolarPlots.m
+++ NG_DriftingGratingCode/PolarPlots.m
@@ -1,5 +1,5 @@
-function [resultantVectors,z] = PolarPlots(outputFolder, cellsToPlot, ...
-                                       showPlots, screenOff, scale, dataMatrix)
+function [resultantVectors] = PolarPlots(outputFolder, cellsToPlot, ...
+                                       showPlots, screenOff, dataMatrix)
 
 % INPUT
 %   outputFolder = name (in SINGLE quotes) of output folder which will be 
@@ -14,16 +14,13 @@ function [resultantVectors,z] = PolarPlots(outputFolder, cellsToPlot, ...
 %       windows to pop up. They'll still be saved unless 'dont save' is
 %       specified.
 %   screenOff = optional; set to 'screenOff' if the columns for 'screenOff' are
-%       included in the dataMatrix (ie the dataMatrix is N x 11 instead of 
-%       N x 9, where N is the number of cells). The screenOff columns need
+%       included in the dataMatrix (ie the dataMatrix is N x 10 instead of 
+%       N x 8, where N is the number of cells). The screenOff columns need
 %       to be the first and last columns (these will be trimmed away). If
 %       those columns are not present set screenOff to ''.
-%   scale = optional; set the limit of the r axis for the plots (if the
-%       data exceeds rfor a given plot then the axis for that plot only will
-%       be extended). Default is 20.
-%   dataMatrix =  optional; n x 9 (or n x 11) matrix where n is # of cells.
-%       The 9 columns are as follows: -180, -135, -90, -45, 0, 45, 90, 135,
-%       180. Each entry is the firing rate in spikes/s. If dataMatrix is
+%   dataMatrix =  optional; n x 8 (or n x 10) matrix where n is # of cells.
+%       The 8 columns are as follows: -180, -135, -90, -45, 0, 45, 90, 135.
+%       Each entry is the firing rate in spikes/s. If dataMatrix is
 %       left out then user will be prompted for a csv file containing the
 %       matrix (csv should have no labels, just the matrix).
 
@@ -44,9 +41,6 @@ if exist('showPlots', 'var') && strcmp(showPlots, 'dont show')
    set(0,'DefaultFigureVisible','off');
 else
     set(0,'DefaultFigureVisible','on');
-end
-if ~exist('scale', 'var')
-    scale = 20;
 end
 
 %get and read the csv
@@ -83,7 +77,7 @@ if ~strcmpi(outputFolder, 'dont save')
    mkdir(target_folder);
 end
 
-rads = deg2rad([-180,-135,-90,-45,0,45,90,135,180]);
+rads = deg2rad([180,225,270,315,0,45,90,135]);
 
 %set up resultant vector figure
 v = figure('Name', 'Orientation Selectivity', 'NumberTitle','off');
@@ -106,11 +100,9 @@ for c = cellsToPlot
     pax1 = polaraxes;
     thetaticks(pax1,0:15:360);
     rticks(0:2:26);
-    rlim(pax1,'manual');
-    rlim(pax1,[0 scale]);
     
-    %set the r scale by creating white circle, and do the main plot
-    polarplot(pax1, rads, M(c,:));
+    % do the main plot, adding the 180 point back to the end so that things connect
+    polarplot(pax1, [rads pi], [M(c,:) M(c,1)]);
     
     %save the file
     if ~strcmpi(outputFolder, 'dont save')
@@ -121,11 +113,15 @@ for c = cellsToPlot
     c_x = sum( M(c,:) .* cos(rads)) / sum(M(c,:));
     c_y = sum( M(c,:) .* sin(rads)) / sum(M(c,:));
     pref_theta = atan(c_y / c_x);
+    if(c_x < 0)
+        pref_theta = pref_theta+pi;
+    end
+    
     selectivity = sqrt(c_x.^2 + c_y.^2);
     resultantVectors(c, :) = [c rad2deg(pref_theta) selectivity];
     
     %plot orientation selectivity
-    polarplot(paxV, [0 pref_theta], [0 selectivity]);
+    polarscatter(paxV, pref_theta, selectivity,[],'k');
 end
 
 resultantVectors = sortrows(resultantVectors, 3, 'descend');
