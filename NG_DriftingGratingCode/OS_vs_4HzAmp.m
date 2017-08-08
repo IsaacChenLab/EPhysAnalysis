f6c19fb57a19f2ccad37079c5eb5fff691421dcf
NickGoel <nckgoel@gmail.com>
2017-08-08 13:08:15 -0400

    Update OS_vs_4HzAmp.m
    adapt to work with PSD instead of FFT

--- NG_DriftingGratingCode/OS_vs_4HzAmp.m
+++ NG_DriftingGratingCode/OS_vs_4HzAmp.m
@@ -1,5 +1,5 @@
 function cell_theta_OS_4Hz = OS_vs_4HzAmp(outputFolder, numCells, theta,...
-                                          selectivityFile, AC_FFT_file)
+                                          selectivityFile, AC_PSD_file)
 
 % FUNCTION ARGUMENTS
 %   outputFolder = name (in SINGLE quotes) of output folder which will be 
@@ -9,25 +9,25 @@ function cell_theta_OS_4Hz = OS_vs_4HzAmp(outputFolder, numCells, theta,...
 %       should be added to the beginning so that user won't be prompted to
 %       choose output directory later
 %   numCells = number of total cells analyzed in the input data
-%   theta = orientation in degrees that the AC_FFT data corresponds to.
+%   theta = orientation in degrees that the AC_PSD data corresponds to.
 %       Only cells whose preferred orietnation is within 22.5 degrees of theta
 %       will be analyzed
 %   selectivityFile =  optional; the complete path for an
 %       Orientation_Selectivity.mat file (output of PolarPlots.m). If not
 %       given, user will be prompted to browse for the file.
-%   AC_FFT_file =  optional; the complete path for an AC_FFT_analysis.mat
+%   AC_PSD_file =  optional; the complete path for an AC_PSD_analysis.mat
 %       file. If not given, user will be prompted to browse for the file. 
-%       This file is the output of Chen_AC_FFT
+%       This file is the output of Chen_AC_PSD
 
 % PLOTS GENERATED
-%   1. Scatter plot of OS magnitude vs amplitude at 4Hz on FFT
+%   1. Scatter plot of OS magnitude vs amplitude at 4Hz on PSD
 %   --- figures are displayed and saved in the output folder ---
 
 % OUTPUT
 %   Column 1 = cell number
 %   Column 2 = orientation (in degrees) or resultant vector
 %   Column 3 = OS (magnitude of resultant vector), ie x axis
-%   Column 4 = average of top 3 FFT peaks between 3.5 and 4.5 Hz, ie y axis
+%   Column 4 = average of top 3 PSD peaks between 3.5 and 4.5 Hz, ie y axis
 
 % get and read the Orientation Selectivity file
 % this is where resultantVectors comes from
@@ -39,15 +39,15 @@ if ~exist('selectivityFile', 'var')
 end
 load(selectivityFile);
  
-% get and load the AC_FFT.mat file
-% this is where AC_FFT_analysis comes from
-if ~exist('AC_FFT_file', 'var')
-    fprintf("\tSelect AC_FFT file...");
+% get and load the AC_PSD.mat file
+% this is where AC_PSD_analysis comes from
+if ~exist('AC_PSD_file', 'var')
+    fprintf("\tSelect AC_PSD file...");
     [data_file, data_path] = uigetfile('*.mat','Select .mat file...');
     fprintf("Selected!\n");
-    AC_FFT_file = strcat(data_path, data_file);
+    AC_PSD_file = strcat(data_path, data_file);
 end
-load(AC_FFT_file);
+load(AC_PSD_file);
 
 %prompt for file where output should be saved and create folder
 if ~(strcmpi(outputFolder, 'dont save') || outputFolder(1) == '@')
@@ -61,7 +61,7 @@ if ~strcmpi(outputFolder, 'dont save')
 end
 
 % extract data from the input data structures
-Hz_Scores = sortrows(AC_FFT_analysis{numCells+1}.Cell_4HzScore);
+Hz_Scores = sortrows(AC_PSD_analysis{numCells+1}.Cell_4HzScore);
 Select_Scores = sortrows(resultantVectors);
 
 %find the cells that are oriented toward theta
