39a26cf1034ebd7fa62110181c03425aa4a691d8
NickGoel <nckgoel@gmail.com>
2017-08-08 16:37:50 -0400

    Update and rename EvokedGamma.m to EvokedOscillations.m
    added documentation

--- NG_DriftingGratingCode/EvokedOscillations.m
+++ NG_DriftingGratingCode/EvokedOscillations.m
@@ -1,4 +1,29 @@
-function EvokedGamma(outputFolder, channelsToPlot, allChannels, times, screenOnTime, screenOffTime, width)
+function EvokedOscillations(outputFolder, channelsToPlot, CSCData, times, screenTime, duration)
+
+% FUNCTION ARGUMENTS
+%   outputFolder = name (in SINGLE quotes) of output folder which will be 
+%       created, and into which all of the output will be saved. If outputFolder 
+%        = 'dont save' then no .fig or .mat files will be saved and the script
+%       will run much faster. If outputFolder is a complete path, then  '@'
+%       should be added to the beginning so that user won't be prompted to
+%       choose output directory later
+%   channelsToPlot = a vector containing the channels you want to analyze ie 1:32
+%   CSCData = a matrix where each row is the LFP for a channel (should have
+%       32 rows)
+%   times = a single row vector with a time corresponding to each column in
+%       CSCData. The number of columns in CSCData and times should be the
+%       same
+%   screenTime = vector of two numbers. First number is the time the screen
+%       came on. Second number is the time the screen turns off.
+%   duration = the number of seconds before and after the screen was turned
+%       on/off that will be plotted
+
+% PLOTS GENERATED
+%   1. plot of the entire LFP
+%   2. plot at the time when the screen turned on
+%   2. plot at the time when the screen turned off
+
+
 
 if(ispc)
     slash = '\';
@@ -8,39 +33,37 @@ end
 
 %prompt for file where output should be saved and create folder
 if ~(strcmpi(outputFolder, 'dont save') || outputFolder(1) == '@')
-    fprintf('\tSelect folder where output files should be placed...'); 
+    fprintf('\tSelect folder where output files should be placed...');
     target_folder = uigetdir('', 'Select output folder');
     fprintf('Selected!\n');
     target_folder = strcat(target_folder, slash, outputFolder);
     mkdir(target_folder);
 end
 
+screenOnTime = screenTime(1);
+screenOffTime = screenTime(2);
+
+screenOnTime = screenOnTime-times(1);
+screenOffTime = screenOffTime-times(1);
+times = times-times(1);
+sampleRate = (length(times)-1)/(times(end) - times(1));
+
 
 for c = channelsToPlot
     
-    LFP = allChannels(c,:);
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-    screenOnTime = screenOnTime-times(1);
-    screenOffTime = screenOffTime-times(1);
-    times = times-times(1);
-    sampleRate = (length(times)-1)/(times(end) - times(1));
-    
-    times = times(1:length(LFP));
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    LFP = CSCData(c,:);
+    ctimes = times(1:length(LFP));
     
     name = ['GammaOsc_Channel' num2str(c) '_wholeplot'];
     
     wholePlot = figure;
     ax1 = axes;
     hold(ax1,'on');
-    plot(times,LFP);
+    plot(ctimes,LFP);
     line([screenOnTime,screenOnTime], ylim(ax1), 'Color', 'red','LineWidth',2.5);
     line([screenOffTime,screenOffTime], ylim(ax1), 'Color', 'red','LineWidth',2.5);
     
-    xticks(times(1):100:times(end));
+    xticks(ctimes(1):100:ctimes(end));
     
     curtick = get(gca, 'XTick');
     set(gca, 'XTickLabel', cellstr(num2str(curtick(:))));
@@ -52,7 +75,7 @@ for c = channelsToPlot
     ylabel(ax1,'Local Field Potential');
     
     saveas(wholePlot, strcat(target_folder, slash, name, '.fig'));
-
+    
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     for q = 1:2
@@ -60,22 +83,21 @@ for c = channelsToPlot
             focus = screenOnTime;
             name = ['GammaOsc_Channel' num2str(c) '_screenOnPlot'];
             name2 = ' screenOnPlot';
-            
         else
             focus = screenOffTime;
             name = ['GammaOsc_Channel' num2str(c) '_screenOffPlot'];
             name2 = ' screenOffPlot';
-        end 
-   
+        end
+        
         f = figure;
         ax2 = axes;
         hold(ax2,'on');
-        startIndex = floor((focus-width)*sampleRate);
-        stopIndex = floor((focus+width)*sampleRate);
-        plot( times(startIndex : stopIndex),LFP(startIndex : stopIndex));
+        startIndex = floor((focus-duration)*sampleRate);
+        stopIndex = floor((focus+duration)*sampleRate);
+        plot( ctimes(startIndex : stopIndex),LFP(startIndex : stopIndex));
         line([focus,focus], ylim(ax2), 'Color', 'red','LineWidth',2.5);
         
-        xlim([focus-width focus+width]);
+        xlim([focus-duration focus+duration]);
         
         curtick = get(gca, 'XTick');
         set(gca, 'XTickLabel', cellstr(num2str(curtick(:))));
